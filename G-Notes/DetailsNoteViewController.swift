c40ceb7cf5a953917db1d2a2e7fcb8ce81d72484
goknurbulutt <120587181+goknurbulutt@users.noreply.github.com>
2023-09-07 00:37:15 +0300

    Data recording with core data, adding notes, showing, saving buttons and typing keyboard gestures for user convenience.

--- G-Notes/DetailsNoteViewController.swift
+++ G-Notes/DetailsNoteViewController.swift
@@ -16,24 +16,20 @@ class DetailsNoteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        
+        let saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveClicked))
+        navigationItem.rightBarButtonItem = saveButton
+//
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(closeTheKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
-        
-//        klavye dışında herhangi bir yere tıklandığında kapansın.
-        
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveClicked))
-//        save butonunun eklenmesi
     }
     
-    
-    @objc func closeTheKeyboard(){
-        
+    @objc func closeTheKeyboard() {
         view.endEditing(true)
     }
     
-    
-    @objc func saveClicked(){
+    @objc func saveClicked() {
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
@@ -43,23 +39,35 @@ class DetailsNoteViewController: UIViewController {
         note.setValue(noteTextView.text, forKey: "note")
         note.setValue(titleTextField.text, forKey: "title")
         note.setValue(UUID(), forKey: "id")
+
         
         do {
             try context.save()
-            print("kayıt edlildi")
-        }catch{
-            print("hata var")
+            print("saved")
+        } catch {
+            print("there is an error")
         }
+
+    
         
         
+        do {
+            try context.save()
+            print("Saved successfully")
+        } catch let error as NSError {
+            print("Error saving data: \(error.localizedDescription)")
+        }
+
         
+
         
+        NotificationCenter.default.post(name: Notification.Name("dataEntered"), object:nil)
         
+        navigationController?.popViewController(animated: true)
+//        saveden sonra tekrar note list ekranına dönsün
+           
         
+
     }
-    
-    
-
-    
-
 }
+
--- G-Notes/ViewController.swift
+++ G-Notes/ViewController.swift
@@ -6,21 +6,70 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
     @IBOutlet weak var notesTableView: UITableView!
+    var titleSeries = [String]()
+    var idSeries = [UUID]()
+    
+   
     
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonClicked))
 //        add butonunun eklenmesi
+        notesTableView.delegate = self
+        notesTableView.dataSource = self
+        
+        getData()
         
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name:NSNotification.Name(rawValue: "dataEntered") , object: nil)
+    }
+    
+//    alınan verinin gözlemci fonk yardımıyla işlenmesi
+    
+    @objc func getData(){
+
+
+        
+        titleSeries.removeAll(keepingCapacity: false)
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Notes")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject]{
+                if let title = result.value(forKey: "title") as? String {
+                    titleSeries.append(title)
+                    
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    idSeries.append(id)
+                }
+                
+                notesTableView.reloadData()
+            }
+        }catch{
+            print("hata varr")
+        }
+    }
+    
 
     @objc func addButtonClicked (){
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
@@ -29,5 +78,21 @@ class ViewController: UIViewController {
         
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleSeries[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return titleSeries.count
+
+    }
+    
+    
+    
 }
+
+
 
